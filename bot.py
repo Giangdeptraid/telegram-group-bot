3e9d47cbbe07bfe6cfced86395c4e1f49fa3d606
Giangdeptraid <119880836+Giangdeptraid@users.noreply.github.com>
2026-02-05 11:26:40 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
 
-TOKEN = "7714727163:AAHTPb-FdfJ01CN7Gigcvp0ZTKhF1UceQdIY"
+TOKEN = "7714727163:AAHTPb-FdfJ01CN7Gigcvp0ZTKhF1UceQdI"
 
 async def protect(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.message:
@@ -9,10 +9,9 @@ async def protect(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     text = update.message.text or ""
 
-    # Xóa link
     if "http://" in text or "https://" in text:
         await update.message.delete()
 
-app = ApplicationBuilder().token(TOKEN).build()
+app = Application.builder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.ALL, protect))
 app.run_polling()
